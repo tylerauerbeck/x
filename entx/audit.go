f4abc94c4bb0455dcccd6d28bf994b32a2e7dc4f
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2024-01-12 17:14:12 -0500

    test
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- entx/audit.go
+++ entx/audit.go
@@ -15,12 +15,16 @@
 package entx
 
 import (
+	"context"
+	"errors"
+
 	"entgo.io/contrib/entgql"
 	"entgo.io/ent"
 	"entgo.io/ent/schema"
 	"entgo.io/ent/schema/field"
 	"entgo.io/ent/schema/index"
 	"entgo.io/ent/schema/mixin"
+	"go.infratographer.com/x/echojwtx"
 )
 
 // AuditMixin defines an interface of a Mixin that provides the created_at and updated_at timestamp fields
@@ -77,4 +81,51 @@ func (auditMixin) Indexes() []ent.Index {
 		index.Fields("created_by"),
 		index.Fields("updated_by"),
 	}
+}
+
+func (auditMixin) Hooks() []ent.Hook {
+	return []ent.Hook{
+		AuditHook,
+	}
+}
+
+// AuditHook sets and returns the created_at, updated_at, etc., fields
+func AuditHook(next ent.Mutator) ent.Mutator {
+	type AuditLogger interface {
+		// SetCreatedAt(time.Time)
+		// CreatedAt() (v time.Time, exists bool) // exists if present before this hook
+		// SetUpdatedAt(time.Time)
+		// UpdatedAt() (v time.Time, exists bool)
+		SetCreatedBy(string)
+		CreatedBy() (id string, exists bool)
+		SetUpdatedBy(string)
+		UpdatedBy() (id string, exists bool)
+	}
+
+	return ent.MutateFunc(func(ctx context.Context, m ent.Mutation) (ent.Value, error) {
+		ml, ok := m.(AuditLogger)
+		if !ok {
+			return nil, errors.New("chop")
+		}
+
+		actor := "unknown"
+
+		id, ok := ctx.Value(echojwtx.ActorCtxKey).(string)
+		if ok {
+			actor = id
+		}
+
+		switch op := m.Op(); {
+		case op.Is(ent.OpCreate):
+			// ml.SetCreatedAt(time.Now())
+			ml.SetCreatedBy(actor)
+			ml.SetUpdatedBy(actor)
+
+		case op.Is(ent.OpUpdateOne | ent.OpUpdate):
+			// ml.SetUpdatedAt(time.Now())
+			ml.SetUpdatedBy(actor)
+		}
+
+		return next.Mutate(ctx, m)
+	})
 }
